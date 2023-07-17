934e37b18f0850f2526ff8a3f676818271c592d6
KnotEnvy <jwsnyder@gmail.com>
2023-07-17 15:19:03 -0400

    NFT Web3 CardGame

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useGlobalContext } from '../context';
+import { PageHOC } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-white' text-xl>Hello from Home</h1>
+
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>Welcome to Avax Gods <br /> a Web3 NFT Card Game</>,
+  <>Connect your wallet to start playing <br /> the ultimate Web3 Battle Card Game</>
+);
