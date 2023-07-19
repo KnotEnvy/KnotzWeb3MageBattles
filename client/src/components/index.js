95aa6d57f1bdbbfa09c0618e81d51cbb3463c592
KnotEnvy <jwsnyder@gmail.com>
2023-07-19 11:40:08 -0400

    event listeners, battle page, player01, 02 gameload

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,18 @@ import PageHOC from "./PageHOC"
 import CustomInput from "./CustomInput"
 import CustomButton from "./CustomButton"
 import Alert from "./Alert"
+import GameLoad from "./GameLoad"
+import PlayerInfo from './Playerinfo';
+import ActionButton from './ActionButton';
+import Card from "./Card"
 
 export {
     PageHOC,
     CustomInput,
     CustomButton,
     Alert,
+    GameLoad,
+    PlayerInfo,
+    ActionButton,
+    Card,
 }
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,65 +1,69 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { CustomButton, CustomInput, PageHOC } from '../components';
 import { useGlobalContext } from '../context';
-import { PageHOC, CustomInput, CustomButton } from '../components';
 
 const Home = () => {
-  const { contract, walletAddress, setShowAlert } = useGlobalContext();
+  const { contract, walletAddress, gameData, setShowAlert, setErrorMessage } = useGlobalContext();
   const [playerName, setPlayerName] = useState('');
   const navigate = useNavigate();
 
   const handleClick = async () => {
     try {
+      const playerExists = await contract.isPlayer(walletAddress);
 
-      const playerExists = await contract.isPlayer(walletAddress)
-
-      if(!playerExists) {
-        await contract.registerPlayer(playerName, playerName);
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName, { gasLimit: 500000 });
 
         setShowAlert({
           status: true,
-          type: "info",
-          message: `${playerName} is being summoned!`
-        })
+          type: 'info',
+          message: `${playerName} is being summoned!`,
+        });
+
+        setTimeout(() => navigate('/create-battle'), 8000);
       }
     } catch (error) {
-      setShowAlert({
-        status: true,
-        type: "failure",
-        message: "Something went wrong!"
-      })
+      setErrorMessage(error);
     }
-  }
+  };
 
   useEffect(() => {
-    const checkForPlayerToken = async () => {
-      const playerExists = await contract.isPlayer(walletAddress)
-      const playerTokenExists = await contract.isPlayerToken(walletAddress)
+    const createPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
 
-      if(playerExists && playerTokenExists) navigate('/create-battle')
+      if (playerExists && playerTokenExists) navigate('/create-battle');
+    };
+
+    if (contract) createPlayerToken();
+  }, [contract]);
+
+  useEffect(() => {
+    if (gameData.activeBattle) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
     }
-    if(contract) checkForPlayerToken();
-  }, [contract])
+  }, [gameData]);
 
   return (
     walletAddress && (
+      <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your battle name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
 
-    <div className='flex flex-col'>
-      <CustomInput
-        label="name"
-        placeholder="Enter your battle name"
-        value={playerName}
-        handleValueChange={setPlayerName}
-      />
-      <CustomButton 
-        title="Register"
-        handleClick={handleClick}
-        restStyles="mt-6"
-      />
-    </div>
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
     )
-  )
+  );
 };
 
 export default PageHOC(
